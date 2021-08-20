5f757864b9c5238c3508a7b3e5378c84339d7b96
Ithihas Reddy <ithihasreddy@Ithihass-MacBook-Pro.local>
2021-08-21 01:10:45 +0530

    added milesStones

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -3,7 +3,7 @@
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "./pancakeRouter.sol";
+import {IPancakeRouter02} from "./pancakeRouter.sol";
 
 pragma solidity ^0.8.0;
 
@@ -19,7 +19,7 @@ contract CustomisedEscrow {
         address buyer;
         address payable seller;
         bool payType; // true if BNB/false if tokens
-        uint256 amount;
+        uint256[] amounts;
         bool sellerConfirmation;
         bool buyerConfirmation;
         uint256 start;
@@ -31,6 +31,10 @@ contract CustomisedEscrow {
 
     address public owner;
 
+    mapping(address=>uint256) public AddressEscrowMap;
+
+    mapping(address=>uint256) public balances;
+
     address payable private withdrawAddress;
 
     uint256[] disputedEscrows;
@@ -39,7 +43,9 @@ contract CustomisedEscrow {
 
     address public token;
 
-    uint8 public ownerCut; 
+    uint8 public ownerCut;
+
+    uint8 public PoolCut; 
 
     address public liquidityPool;
 
@@ -64,7 +70,7 @@ contract CustomisedEscrow {
         address buyer,
         address payable seller,
         bool payType,
-        uint256 amount,
+        uint256[] amounts,
         uint256 start,
         uint256 timeInDays,
         State currentState
@@ -102,26 +108,38 @@ contract CustomisedEscrow {
         timeLimitInDays = _timeLimitInDays;
     }
 
-    function createEscrowBNB(address payable _seller,uint256 amt,uint256 timeInDays) public payable{
-        require(msg.value >= amt);
+    function arraySum(uint256[] memory array) internal pure returns (uint256) {
+        require(array.length >1);
+        uint256 sum = 0;
+        for (uint256 i = 0; i < array.length-1; i++) {
+            sum = SafeMath.add(sum,array[i]);
+        }
+        return sum;
+    }
+
+    function createEscrowBNB(address payable _seller,uint256[] memory amts,uint256 timeInDays) public payable{
+        require(msg.value >= arraySum(amts));
         require(timeInDays <= timeLimitInDays,"timePeriod more than limit");
         totalEscrows++;
         uint256 id = totalEscrows;
-        getEscrow[id]= instance(id,msg.sender,_seller,true,amt,false,false,block.timestamp,timeInDays,State.initiated);
+        getEscrow[id]= instance(id,msg.sender,_seller,true,amts,false,false,block.timestamp,timeInDays,State.initiated);
         escrowAmtsBNB[id] = msg.value;
+        AddressEscrowMap[msg.sender] = id;
         approvedForWithdraw[id] = false;
-        emit EscrowCreated(id,msg.sender,_seller,true,amt,block.timestamp,timeInDays,State.initiated);
+        emit EscrowCreated(id,msg.sender,_seller,true,amts,block.timestamp,timeInDays,State.initiated);
     }
 
-    function createEscrowToken(address payable _seller,uint256 amt,uint256 timeInDays) public {
+    function createEscrowToken(address payable _seller,uint256[] memory amts,uint256 timeInDays) public {
+        uint amt = arraySum(amts);
         require(IERC20(token).balanceOf(msg.sender) >= amt);
         require(IERC20(token).transferFrom(msg.sender,address(this),amt));
         require(timeInDays <= timeLimitInDays,"timePeriod more than limit");
         totalEscrows++;
         uint256 id = totalEscrows;
-        getEscrow[id]= instance(id,msg.sender,_seller,false,amt,false,false,block.timestamp,timeInDays,State.initiated);
+        getEscrow[id]= instance(id,msg.sender,_seller,false,amts,false,false,block.timestamp,timeInDays,State.initiated);
         escrowAmtsToken[id] = amt;
-        emit EscrowCreated(id,msg.sender,_seller,false,amt,block.timestamp,timeInDays,State.initiated);
+        AddressEscrowMap[msg.sender] = id;
+        emit EscrowCreated(id,msg.sender,_seller,false,amts,block.timestamp,timeInDays,State.initiated);
     }
 
     function updateDelivery(uint256 _id) public onlyBuyer(_id){
@@ -129,13 +147,19 @@ contract CustomisedEscrow {
         require(getEscrow[_id].sellerConfirmation,"Seller has not Confirmed delivery");
         require(!getEscrow[_id].buyerConfirmation,"Buyer already confirmed");
         require(!approvedForWithdraw[_id]);
+        delete getEscrow[_id];
+        uint256 _PoolCut = ceilDiv(SafeMath.mul(PoolCut,escrowAmtsBNB[_id]),1000);
         if(getEscrow[_id].payType){
             uint256 OwnerCut = ceilDiv(SafeMath.mul(ownerCut,escrowAmtsBNB[_id]),1000);
-            getEscrow[_id].seller.sendValue(escrowAmtsBNB[_id]-OwnerCut);
+            uint256 temp= escrowAmtsBNB[_id];
+            escrowAmtsBNB[_id] = 0;
+            getEscrow[_id].seller.sendValue(temp-OwnerCut-_PoolCut);
             withdrawAddress.sendValue(OwnerCut);
         }else if(!getEscrow[_id].payType){
             uint256 OwnerCut = ceilDiv(SafeMath.mul(ownerCut,escrowAmtsToken[_id]),1000);
-            IERC20(token).transfer(getEscrow[_id].seller,escrowAmtsToken[_id]-OwnerCut);
+            uint256 temp = escrowAmtsToken[_id];
+            escrowAmtsToken[_id] = 0;   
+            IERC20(token).transfer(getEscrow[_id].seller,temp-OwnerCut-_PoolCut);
             IERC20(token).transfer(withdrawAddress,OwnerCut);
         }
         getEscrow[_id].buyerConfirmation=true;
@@ -181,6 +205,10 @@ contract CustomisedEscrow {
     function ceilDiv(uint256 a, uint256 b) internal pure returns (uint256) {
         // (a + b - 1) / b can overflow on addition, so we distribute.
         return a / b + (a % b == 0 ? 0 : 1);
+    }
+
+    receive() external payable {
+        balances[msg.sender] += msg.value;
     }
 
 }
